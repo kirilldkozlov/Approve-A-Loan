37b4edd53c7bfeca3f0f143a24c715194922594d
kirilldkozlov <kirill.d.kozlov@gmail.com>
2018-01-18 00:13:07 -0500

    better nil case

--- app/controllers/analyzer_api_controller.rb
+++ app/controllers/analyzer_api_controller.rb
@@ -36,8 +36,8 @@ class AnalyzerApiController < ApplicationController
 
   def build_logs(name)
     first_name, last_name = name.split
-    first_name = first_name.nil? ? '/' : first_name
-    last_name = last_name.nil? ? '/' : last_name
+    first_name = first_name.nil? ? "#{SecureRandom.base64}" : first_name
+    last_name = last_name.nil? ? "#{SecureRandom.base64}" : last_name
 
     Log.saved.where(
       '(name= ?) OR
