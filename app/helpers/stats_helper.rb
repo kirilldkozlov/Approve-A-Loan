c5c7636109afaaf415070e1446a85ab181519e8f
kirilldkozlov <kirill.d.kozlov@gmail.com>
2018-01-18 13:19:22 -0500

    Tweaks for tests

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,7 +1,7 @@
 module StatsHelper
   def self.sum_of_loans
     arr = Profile::APPROVED_CURRENCY.map do |cur|
-      sum = Log.where(currency: cur, verdict: 'approved').sum(:loan_amount)
+      sum = Log.saved.where(currency: cur, verdict: 'approved').sum(:loan_amount)
 
       format('%0.2f', sum).to_s + ' ' + cur if sum > 0
     end
