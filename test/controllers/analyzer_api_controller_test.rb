066743d2ecc9a3e49742634ee1d796a54833591a
kirilldkozlov <kirill.d.kozlov@gmail.com>
2019-05-20 15:09:55 -0400

    Modernize test coverage

--- test/controllers/analyzer_api_controller_test.rb
+++ test/controllers/analyzer_api_controller_test.rb
@@ -13,28 +13,28 @@ class AnalyzerApiControllerTest < ActionDispatch::IntegrationTest
   end
 
   test '#logs returns an error if there is no matchs' do
-    get logs_path('Not a name')
+    get logs_path(name: 'Not a name'), as: :json, xhr: true
     assert_response :success
 
     assert JSON.parse(response.body)['error']
   end
 
   test '#logs returns by a single first name' do
-    get logs_path(names.first)
+    get logs_path(name: names.first), as: :json, xhr: true
     assert_response :success
 
     assert_equal @log.name, JSON.parse(response.body).first['name']
   end
 
   test '#logs returns by a single last name' do
-    get logs_path(names.last)
+    get logs_path(name: names.last), as: :json, xhr: true
     assert_response :success
 
     assert_equal @log.name, JSON.parse(response.body).first['name']
   end
 
   test '#logs returns by 3 letters of first name' do
-    get logs_path(names.first[0..2])
+    get logs_path(name: names.first[0..2]), as: :json, xhr: true
     assert_response :success
 
     assert_equal @log.name, JSON.parse(response.body).first['name']
